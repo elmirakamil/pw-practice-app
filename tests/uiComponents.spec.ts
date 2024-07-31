4e2c7f975616b323b5446f82d715c430fe083dcb
elmirakamil <eashimova85@gmail.com>
2024-07-31 10:17:08 -0500

    Webtables, tooltips

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -53,7 +53,103 @@ test.describe('Form Layouts page', () => {
             await box.uncheck({ force: true })
             expect(await box.isChecked()).toBeFalsy()
         }
-
     })
+})
 
+test('lists and dropdowns', async ({ page }) => {
+    const dropDownMenu = page.locator('ngx-header nb-select')
+    await dropDownMenu.click()
+
+    page.getByRole('list') //when the list has a UL tag
+    page.getByRole('listitem') //when the list has LI tag
+
+    // const optionList = page.getByRole('list').locator('nb-option')
+    const optionList = page.locator('nb-option-list nb-option')
+    await expect(optionList).toHaveText(["Light", "Dark", "Cosmic", "Corporate"])
+    await optionList.filter({ hasText: "Cosmic" }).click()
+    const header = page.locator('nb-layout-header')
+    await expect(header).toHaveCSS('background-color', 'rgb(50, 50, 89)')
+
+    const colors = {
+        "Light": "rgb(255, 255, 255)",
+        "Dark": "rgb(34, 43, 69)",
+        "Cosmic": "rgb(50, 50, 89)",
+        "Corporate": "rgb(255, 255, 255)"
+    }
+
+    await dropDownMenu.click()
+    for (const color in colors) {
+        await optionList.filter({ hasText: color }).click()
+        await expect(header).toHaveCSS('background-color', colors[color])
+        if (color != "Corporate")
+            await dropDownMenu.click()
+    }
+
+})
+
+test('tooltips', async ({ page }) => {
+    await page.getByText('Modal & Overlays').click()
+    await page.getByText('Tooltip').click()
+
+    const tooltipCard = page.locator('nb-card', { hasText: "Tooltip Placements" })
+    await tooltipCard.getByRole('button', { name: "Top" }).hover()
+
+    page.getByRole('tooltip') //if you have a role tooltip created
+    const tooltip = await page.locator('nb-tooltip').textContent()
+    expect(tooltip).toEqual('This is a tooltip')
+})
+
+test('web dialog', async ({page}) =>{
+    await page.getByText('Tables & Data').click()
+    await page.getByText('Smart Table').click()
+
+    page.on('dialog', dialog =>{
+        expect(dialog.message()).toEqual("are you sure you want to delete?")
+        dialog.accept()
+    })
+    await page.getByRole('table').locator('tr', {hasText:"mdo@gmail.com"}).click()
+    await expect(page.locator('table tr').first()).not.toHaveText("mdo@gmail.com")
+
+})
+
+test('web tables', async({page}) => {
+    await page.getByText('Tables & Data').click()
+    await page.getByText('Smart Table').click()
+
+    //1 get the row by any test in this row
+    const targetRow = page.getByRole('row', {name: "twitter@outlook.com"})
+    await targetRow.locator('.nb-edit').click()
+    await page.locator('input-editor').getByPlaceholder('Age').clear()
+    await page.locator('input-editor').getByPlaceholder('Age').fill('35')
+    await page.locator('.nb-checkmark').click()
+
+    //2 get the row based on the value in the specific column
+    await page.locator('.ng2-smart-pagination-nav').getByText('2').click()
+    const targetRowById = page.getByRole('row', {name: "11"}).filter({has: page.locator('td').nth(1).getByText('11')})
+    await targetRowById.locator('.nb-edit').click()
+    await page.locator('input-editor').getByPlaceholder('E-mail').clear()
+    await page.locator('input-editor').getByPlaceholder('E-mail').fill('test@test.com')
+    await page.locator('.nb-checkmark').click()
+    await expect(targetRowById.locator('td').nth(5)).toHaveText('test@test.com')
+
+    //3 test filter of the table
+
+    const ages = ["20", "30", "40", "200"]
+
+    for( let age of ages){
+        await page.locator('input-filter').getByPlaceholder('Age').clear()
+        await page.locator('input-filter').getByPlaceholder('Age').fill(age)
+        await page.waitForTimeout(500)
+        const ageRows = page.locator('tbody tr')
+        
+        for(let row of await ageRows.all()){
+            const cellValue = await row.locator('td').last().textContent()
+
+            if(age == "200"){
+                expect(await page.getByRole('table').textContent()).toContain('No data found')
+            } else {
+                expect(cellValue).toEqual(age)
+            }
+        }
+    }
 })
